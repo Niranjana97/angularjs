c70d6c3cba0f70fb140dd0c0eb4265aa45e551ac
Niranjana97 <sniranjanamvk@gmail.com>
2016-09-23 19:05:23 +0530

    angularjs project

---
+++ ShoppingCart/js/product.js
@@ -0,0 +1,10 @@
+﻿//----------------------------------------------------------------
+// product class
+function product(sku, name, price, rat) {
+    this.sku = sku; // product code (SKU = stock keeping unit)
+    this.name = name;
+   // this.description = description;
+    this.price = price;
+    this.rat = rat;
+    
+}
---
+++ ShoppingCart/js/store.js
@@ -0,0 +1,24 @@
+﻿//----------------------------------------------------------------
+// store (contains the products)
+
+function store() {
+    this.products = [
+        new product("phone1", "Gionee s6", 130.6, 90),
+        new product("phone2", "Lenovo K3 note", 150, 90),
+        new product("phone3", "Lenovo vibe", 143.5, 120),
+        new product("phone4", "Samsung galaxy", 110.3, 50),
+        new product("phone5", "Moto G3 ", 160.77, 100),
+        new product("phone6", "Micromax Canvas ", 165.33, 50),
+        new product("phone7", "Acer Liquid Z530", 149.43, 100),
+        new product("phone8", "Acer Liquid E3", 154.39, 50),
+       
+    ];
+    
+}
+store.prototype.getProduct = function (sku) {
+    for (var i = 0; i < this.products.length; i++) {
+        if (this.products[i].sku == sku)
+            return this.products[i];
+    }
+    return null;
+}
